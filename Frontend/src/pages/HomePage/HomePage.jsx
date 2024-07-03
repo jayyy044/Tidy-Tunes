21c0cbe24082c6557c62ff26c0eb0ac91a7c8c04
Maanas <maanasksaxena@gmail.com>
2024-07-03 09:55:17 -0600

    fixes

--- Frontend/src/pages/HomePage/HomePage.jsx
+++ Frontend/src/pages/HomePage/HomePage.jsx
@@ -1,58 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-import './HomePage.css'
-import headphonesPng from '../../assets/headphones_removed.png'
+import { Link } from 'react-router-dom';
+import './HomePage.css';
+import headphonesPng from '../../assets/headphones_removed.png';
 
 const HomePage = () => {
-
+  
   return (
-    <>
-              {/* <h2>Our Mission</h2>
-        <h4>At Tidy Tunes, our mission is to enhance your 
-          music listening experience by helping you curate 
-          the perfect playlist. We monitor your 
-          listening habits and provide personalized suggestions 
-          to keep your playlist fresh and enjoyable. 
-          Additionally, we keep an eye on your favorite 
-          artists, analyzing their new releases to rank 
-          songs based on your unique preferences. 
-          With Tidy Tunes, you'll always have a 
-          playlist that resonates with your musical 
-          taste, ensuring every listening session is a joy.
-        </h4>
-        <h2>Our Mission</h2>
-        <h4>At Tidy Tunes, our mission is to enhance your 
-          music listening experience by helping you curate 
-          the perfect playlist. We monitor your 
-          listening habits and provide personalized suggestions 
-          to keep your playlist fresh and enjoyable. 
-          Additionally, we keep an eye on your favorite 
-          artists, analyzing their new releases to rank 
-          songs based on your unique preferences. 
-          With Tidy Tunes, you'll always have a 
-          playlist that resonates with your musical 
-          taste, ensuring every listening session is a joy.
-        </h4>
-        </div>
-          <img src={headphonesPng}/>
-        </div> */}
-      <div className="HomeContainer">
-        <div className="Homepage_Welcome">
-          hello
-          {/* <p id='hh'>Welcome to Tidy Tunes</p>
-          <h3>Redefine Your Music Listening Experience</h3> */}
-        </div>
-        <div className="image">
-            <img src={headphonesPng}/>
-        </div>
-        <div className="HomePage_content">
-          helllo2
-        </div>
+    <div className="HomeContainer">
+      <div className="HomePage_Welcome">
+        <h1>
+          <span>Welcome to</span> 
+          <span className='part2'>Tidy Tunes</span> 
+        </h1>
+        <p>Redefine Your Music Listening Experience</p>
+      </div>
+      <div className="HomePage_Image">
+        <img src={headphonesPng} alt="Headphones" />
       </div>
       
-    </>
+      <div className="HomePage_Content">
+        <h1>Our Mission</h1>
+        <p>
+          At Tidy Tunes, our mission is to enhance your 
+          music listening experience by helping you culivate 
+          your music. We gaurantee you'll always have a playlist that 
+           resonates with your musical taste, ensuring every listening session is a joy.
+        </p>
+        <Link to='/register'>Get Started</Link>
+      </div>
+    </div>
   );
-}
+};
 
 export default HomePage;
-
