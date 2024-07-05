c11fd3f626404991c9ef03557f6a350e68f18a01
Maanas <maanasksaxena@gmail.com>
2024-07-04 23:03:43 -0600

    frontpage responsive

--- Frontend/src/components/NavBarOUT/NavBarOut.jsx
+++ Frontend/src/components/NavBarOUT/NavBarOut.jsx
@@ -1,28 +1,85 @@
-import React from 'react'
+import React, {useRef, useState, useEffect} from 'react'
 import './NavBarOut.css'
 import { Link, NavLink } from 'react-router-dom'
+import { Drawer } from 'antd'
+import MediaQuery from 'react-responsive'
+import { TfiAlignJustify } from "react-icons/tfi";
+import LogoImg from '../../assets/TidyTunes_Logo.png'
 
 const NavBarOut = () => {
+  const elementRef = useRef(null);
+  const [height, setHeight] = useState(0);
+  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    const updateHeight = () => {
+      if (elementRef.current) {
+        const rect = elementRef.current.getBoundingClientRect();
+        setHeight(rect.height);
+        document.documentElement.style.setProperty('--navbarHeight', `${rect.height}px`);
+      }
+    };
+
+    // Update height initially
+    updateHeight();
+
+    // Set up a resize observer to update height on window resize
+    const resizeObserver = new ResizeObserver(updateHeight);
+    resizeObserver.observe(elementRef.current);
+
+    // Clean up the observer on component unmount
+    return () => {
+      if (elementRef.current) {
+        resizeObserver.unobserve(elementRef.current);
+      }
+    };
+  }, []);
+  const showDrawer = () => {
+    setOpen(true);
+  };
+  const closeDrawer = () => {
+    setOpen(false);
+  };
+
   return (
     <>
-      <nav className='out'>
+      <nav ref= {elementRef} className='NavContainer'>
+        <Link to ='/' className='LogoImage'>
+          <img src={LogoImg} alt='Logo Image'/>
+        </Link>
         <Link to = '/' className='homeButton'>
             <h2>
                 Tidy Tunes
             </h2>
         </Link>
-        <div className="user">
-          <NavLink to= '/login' >
-            <h2>
-              Login
-            </h2>
-          </NavLink>
-          <NavLink to='/register' >
-            <h2>
-              Sign Up
-            </h2>
-          </NavLink>
-        </div>
+        <MediaQuery minWidth={750}>
+          <div className="user">
+            <NavLink to= '/login' >
+              <h2>
+                Login
+              </h2>
+            </NavLink>
+            <NavLink to='/register' >
+              <h2>
+                Sign Up
+              </h2>
+            </NavLink>
+          </div>
+        </MediaQuery>
+        <MediaQuery maxWidth={749}>
+          <TfiAlignJustify className= 'icon' onClick={showDrawer}/>
+          <Drawer title="Get Started" onClose={closeDrawer} open={open} className='drawer'>
+            <NavLink to= '/login' onClick={closeDrawer} >
+                <h2>
+                  Login
+                </h2>
+              </NavLink>
+              <NavLink to='/register' onClick={closeDrawer} >
+                <h2>
+                  Sign Up
+                </h2>
+              </NavLink>
+          </Drawer>
+        </MediaQuery>
       </nav>
     </>
   )
--- Frontend/src/pages/HomePage/HomePage.jsx
+++ Frontend/src/pages/HomePage/HomePage.jsx
@@ -1,34 +1,59 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 import './HomePage.css';
+import MediaQuery from 'react-responsive'
 import headphonesPng from '../../assets/headphones_removed.png';
+import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
-  
   return (
-    <div className="HomeContainer">
-      <div className="HomePage_Welcome">
-        <h1>
-          <span>Welcome to</span> 
-          <span className='part2'>Tidy Tunes</span> 
-        </h1>
-        <p>Redefine Your Music Listening Experience</p>
+    <>
+      <div className="HomeContainer">
+
+        <div className="HomePage_Welcome">
+          <h1>
+            <span>Welcome to</span> 
+            <span className='part2'>Tidy Tunes</span> 
+          </h1>
+          <p>Redefine Your Music Listening Experience</p>
+          <MediaQuery maxWidth={1199}>
+            <Link to='/register'>Get Started</Link>
+          </MediaQuery>
+        </div>
+        <MediaQuery minWidth={750}>
+          <div className="HomePage_Image">
+            <img src={headphonesPng} alt="Headphones" />
+          </div>
+        </MediaQuery>
+        <MediaQuery minWidth={1200} className='hello'>
+          <div className="HomePage_Content">
+            <h1>Our Mission</h1>
+            <p>
+              At Tidy Tunes, our mission is to enhance your 
+              music listening experience by helping you culivate 
+              your music. We gaurantee you'll always have a playlist that 
+              resonates with your musical taste, ensuring every listening session is a joy.
+            </p>
+            <Link to='/register'>Get Started</Link>
+          </div>
+        </MediaQuery>
       </div>
+      <MediaQuery maxWidth={749}>
       <div className="HomePage_Image">
-        <img src={headphonesPng} alt="Headphones" />
-      </div>
-      
+          <img src={headphonesPng} alt="Headphones" />
+        </div>
+      </MediaQuery>
+      <MediaQuery maxWidth={1199}>
       <div className="HomePage_Content">
-        <h1>Our Mission</h1>
-        <p>
-          At Tidy Tunes, our mission is to enhance your 
-          music listening experience by helping you culivate 
-          your music. We gaurantee you'll always have a playlist that 
-           resonates with your musical taste, ensuring every listening session is a joy.
-        </p>
-        <Link to='/register'>Get Started</Link>
-      </div>
-    </div>
+            <h1>Our Mission</h1>
+            <p>
+              At Tidy Tunes, our mission is to enhance your 
+              music listening experience by helping you culivate 
+              your music. We gaurantee you'll always have a playlist that 
+              resonates with your musical taste, ensuring every listening session is a joy.
+            </p>
+          </div>
+      </MediaQuery>
+    </>
   );
 };
 
