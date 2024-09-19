d67f8f8ebce12d538f80ac2eb33cf3f575d2ee20
Maanas <maanasksaxena@gmail.com>
2024-09-19 13:55:16 -0600

    final commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
     port: 4000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        // target: 'http://localhost:3000',
+        target: 'http://18.223.122.149:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
